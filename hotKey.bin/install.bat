72ca51bb18773c4112ba161e67c2b95fbdf7a199
ms <ms@skbkontur.ru>
2013-10-22 01:21:13 +0600

    binary

---
+++ hotKey.bin/install.bat
@@ -0,0 +1,2 @@
+copy hokey.exe "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+start "" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\hokey.exe"
