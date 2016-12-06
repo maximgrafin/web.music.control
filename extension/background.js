d93c6bb539f162a701484ea8f0fd1c9b8f257fa9
Sadym <sadym@amazon.com>
2016-12-06 23:15:36 +0100

    google play music

--- extension/background.js
+++ extension/background.js
@@ -1,198 +1,216 @@
 var services = [{
-    prefix: 'vk.com',
-    isPlaying: 'document.getElementsByClassName(\'top_audio_player_playing\').length > 0',
-    toggle: 'document.getElementsByClassName(\'top_audio_player_play\')[0].click();',
-    next: 'document.getElementsByClassName(\'top_audio_player_next\')[0].click();',
-    prev: 'document.getElementsByClassName(\'top_audio_player_prev\')[0].click();',
-    manipulateFirst: true
+	prefix: 'vk.com',
+	isPlaying: 'document.getElementsByClassName(\'top_audio_player_playing\').length > 0',
+	toggle: 'document.getElementsByClassName(\'top_audio_player_play\')[0].click();',
+	next: 'document.getElementsByClassName(\'top_audio_player_next\')[0].click();',
+	prev: 'document.getElementsByClassName(\'top_audio_player_prev\')[0].click();',
+	manipulateFirst: true
 }, {
-    prefix: 'www.mixcloud.com',
-    isPlaying: 'document.getElementsByClassName(\'pause-state\').length > 0',
-    toggle: 'document.getElementsByClassName(\'player-control\')[0].click();'
+	prefix: 'play.google.com/music',
+	isPlaying: 'document.getElementById(\'player-bar-play-pause\').className.indexOf(\'playing\')>=0',
+	toggle: 'document.getElementById(\'player-bar-play-pause\').click();',
+	next: 'document.getElementById(\'player-bar-forward\').click();',
+	prev: 'document.getElementById(\'player-bar-rewind\').click();'
 }, {
-    prefix: 'www.silver.ru',
-    isPlaying: '(!document.getElementsByTagName("audio")[0].paused)',
-    play: 'document.getElementsByTagName("audio")[0].play()',
-    pause: 'document.getElementsByTagName("audio")[0].pause()'
+	prefix: 'www.mixcloud.com',
+	isPlaying: 'document.getElementsByClassName(\'pause-state\').length > 0',
+	toggle: 'document.getElementsByClassName(\'player-control\')[0].click();'
 }, {
-    prefix: 'www.periscope.tv',
-    isPlaying: 'document.getElementsByClassName(\'is-playing\').length > 0',
-    toggle: 'document.getElementsByClassName(\'PlaybackControls\')[0].click();'
+	prefix: 'www.silver.ru',
+	isPlaying: '(!document.getElementsByTagName("audio")[0].paused)',
+	play: 'document.getElementsByTagName("audio")[0].play()',
+	pause: 'document.getElementsByTagName("audio")[0].pause()'
 }, {
-    prefix: 'soundcloud.com',
-    isPlaying: 'document.getElementsByClassName(\'playControl\').length > 0 && document.getElementsByClassName(\'playControl\')[0].className.indexOf(\'playing\')>=0',
-    toggle: 'document.getElementsByClassName(\'playControl\')[0].click();'
+	prefix: 'www.periscope.tv',
+	isPlaying: 'document.getElementsByClassName(\'is-playing\').length > 0',
+	toggle: 'document.getElementsByClassName(\'PlaybackControls\')[0].click();'
 }, {
-    prefix: 'www.youtube.com',
-    isPlaying: 'document.getElementsByClassName(\'html5-video-player\').length > 0 && document.getElementsByClassName(\'html5-video-player\')[0].className.indexOf(\'playing-mode\')>=0',
-    toggle: 'document.getElementsByClassName(\'ytp-play-button\')[0].click();'
+	prefix: 'soundcloud.com',
+	isPlaying: 'document.getElementsByClassName(\'playControl\').length > 0 && document.getElementsByClassName(\'playControl\')[0].className.indexOf(\'playing\')>=0',
+	toggle: 'document.getElementsByClassName(\'playControl\')[0].click();'
 }, {
-    prefix: 'play.spotify.com',
-    isPlaying: 'document.getElementById(\'play-pause\') && document.getElementById(\'play-pause\').className.indexOf(\'playing\')>=0',
-    toggle: 'document.getElementById(\'play-pause\').click();',
-    next: 'document.getElementById(\'next\').click();',
-    prev: 'document.getElementById(\'previous\').click();',
-    frameUrl: "play.spotify.com/apps/player"
+	prefix: 'www.youtube.com',
+	isPlaying: 'document.getElementsByClassName(\'html5-video-player\').length > 0 && document.getElementsByClassName(\'html5-video-player\')[0].className.indexOf(\'playing-mode\')>=0',
+	toggle: 'document.getElementsByClassName(\'ytp-play-button\')[0].click();'
+}, {
+	prefix: 'play.spotify.com',
+	isPlaying: 'document.getElementById(\'play-pause\') && document.getElementById(\'play-pause\').className.indexOf(\'playing\')>=0',
+	toggle: 'document.getElementById(\'play-pause\').click();',
+	next: 'document.getElementById(\'next\').click();',
+	prev: 'document.getElementById(\'previous\').click();',
+	frameUrl: "play.spotify.com/apps/player"
+}, {
+	prefix: '.podomatic.com',
+	isPlaying: 'document.getElementsByClassName(\'audio_play\').length > 0 && document.getElementsByClassName(\'audio_play\')[0].className.indexOf(\'pause\')>=0',
+	toggle: 'document.getElementsByClassName(\'audio_prev\')[0].click();',
+	next: 'document.getElementsByClassName(\'audio_play\')[0].click();',
+	prev: 'document.getElementsByClassName(\'audio_next\')[0].click();'
 }];
 
 function executeOnFrame(tabId, code, frameId, callBack) {
-    chrome.tabs.executeScript(tabId, {code: code, frameId: frameId || 0}, function(args) {
-        if (callBack) {
-            callBack(args[0]);
-        }
-    });
+	chrome.tabs.executeScript(tabId, {code: code, frameId: frameId || 0}, function (args) {
+		if (callBack) {
+			callBack(args[0]);
+		}
+	});
 }
 
 function getFrameId(tabId, frameUrl, callBack) {
-    if (!frameUrl) {
-        return callBack(0);
-    }
+	if (!frameUrl) {
+		return callBack(0);
+	}
 
-    chrome.webNavigation.getAllFrames({tabId: tabId}, function(frames) {
-        for (var i in frames) {
-            var frame = frames[i];
-            if (startsWith(frame.url, frameUrl)) {
-                callBack(frame.frameId);
-            }
-        }
-    });
+	chrome.webNavigation.getAllFrames({tabId: tabId}, function (frames) {
+		for (var i in frames) {
+			var frame = frames[i];
+			if (startsWith(frame.url, frameUrl)) {
+				callBack(frame.frameId);
+			}
+		}
+	});
 }
 
 function executeScript(tabId, code, frameUrl, callBack) {
-    getFrameId(tabId, frameUrl, function(frameId) {
-        executeOnFrame(tabId, code, frameId, callBack);
-    });
+	getFrameId(tabId, frameUrl, function (frameId) {
+		executeOnFrame(tabId, code, frameId, callBack);
+	});
 }
 
 function pause(tabId, service) {
-    if (service.hasOwnProperty("toggle")) {
-        executeScript(tabId, service.toggle, service.frameUrl);
-    } else {
-        executeScript(tabId, service.pause, service.frameUrl);
-    }
+	if (service.hasOwnProperty("toggle")) {
+		executeScript(tabId, service.toggle, service.frameUrl);
+	} else {
+		executeScript(tabId, service.pause, service.frameUrl);
+	}
 }
 
 function next(tabId, service) {
-    if (service.hasOwnProperty("next")) {
-        executeScript(tabId, service.next, service.frameUrl);
-    } else {
-        console.log("service " + service.prefix + " does not support next");
-    }
+	if (service.hasOwnProperty("next")) {
+		executeScript(tabId, service.next, service.frameUrl);
+	} else {
+		console.log("service " + service.prefix + " does not support next");
+	}
 }
 
 function prev(tabId, service) {
-    if (service.hasOwnProperty("prev")) {
-        executeScript(tabId, service.prev, service.frameUrl);
-    } else {
-        console.log("service " + service.prefix + " does not support prev");
-    }
+	if (service.hasOwnProperty("prev")) {
+		executeScript(tabId, service.prev, service.frameUrl);
+	} else {
+		console.log("service " + service.prefix + " does not support prev");
+	}
 }
 
 function play(tabId, service) {
-    if (service.hasOwnProperty("toggle")) {
-        executeScript(tabId, service.toggle, service.frameUrl);
-    } else {
-        executeScript(tabId, service.play, service.frameUrl);
-    }
+	if (service.hasOwnProperty("toggle")) {
+		executeScript(tabId, service.toggle, service.frameUrl);
+	} else {
+		executeScript(tabId, service.play, service.frameUrl);
+	}
 }
 
 function getIsPlaying(tabId, service, callBack) {
-    executeScript(tabId, service.isPlaying, service.frameUrl, callBack);
+	executeScript(tabId, service.isPlaying, service.frameUrl, callBack);
 }
 
 function startsWith(origin, subStr) {
-    var flag = origin.indexOf(subStr);
-    return flag >= 0 && flag <= 10;
+	var flag = origin.indexOf(subStr);
+	return flag >= 0 && flag <= 100;
 }
 
 function foreachTab(callback) {
-    chrome.tabs.query({}, function(tabs) {
-        //console.log(tabs);
-        for (var i in services) {
-            services[i].visited = false;
-        }
+	chrome.tabs.query({}, function (tabs) {
+		//console.log(tabs);
+		for (var i in services) {
+			services[i].visited = false;
+		}
 
-        var first = true;
-        for (var i in tabs) {
-            var tab = tabs[i];
+		var first = true;
+		for (var i in tabs) {
+			var tab = tabs[i];
 
-            var _debug = {};
+			var _debug = {};
 
-            for (var i in services) {
-                var service = services[i];
+			for (var i in services) {
+				var service = services[i];
 
-                if (startsWith(tab.url, service.prefix)) {
-                    (function() {
-                        var tabId = tab.id;
-                        var _service = services[i];
-                        var _first = first;
-                        var _firstSame = !_service.visited;
-                        _service.visited = true;
+				if (startsWith(tab.url, service.prefix)) {
+					(function () {
+						var tabId = tab.id;
+						var _service = services[i];
+						var _first = first;
+						var _firstSame = !_service.visited;
+						_service.visited = true;
 
-                        getIsPlaying(tabId, _service, function(isPlaying) {
-                            callback(tabId, _service, _first, _firstSame, isPlaying);
-                        });
-                    })();
-                    first = false;
-                }
-            }
-        }
-    });
+						getIsPlaying(tabId, _service, function (isPlaying) {
+							callback(tabId, _service, _first, _firstSame, isPlaying);
+						});
+					})();
+					first = false;
+				}
+			}
+		}
+	});
 }
 
 function togglePause() {
-    foreachTab(function(tabId, _service, _first, _firstSame, isPlaying) {
-        if (_service.manipulateFirst && !_firstSame) {
-            return;
-        }
+	foreachTab(function (tabId, _service, _first, _firstSame, isPlaying) {
+		if (_service.manipulateFirst && !_firstSame) {
+			return;
+		}
 
-        if (isPlaying) {
-            pause(tabId, _service);
-        }
+		if (isPlaying) {
+			pause(tabId, _service);
+		}
 
-        if (_first && !isPlaying) {
-            play(tabId, _service);
-        }
-    });
+		if (_first && !isPlaying) {
+			play(tabId, _service);
+		}
+	});
 }
 
 function nextTrack() {
-    foreachTab(function(tabId, _service, _first, _firstSame, isPlaying) {
-        if (!isPlaying) {
-            return;
-        }
-        next(tabId, _service);
-    });
+	foreachTab(function (tabId, _service, _first, _firstSame, isPlaying) {
+		if (!isPlaying) {
+			return;
+		}
+		next(tabId, _service);
+	});
 }
 
 function prevTrack() {
-    foreachTab(function(tabId, _service, _first, _firstSame, isPlaying) {
-        if (!isPlaying) {
-            return;
-        }
-        prev(tabId, _service);
-    });
+	foreachTab(function (tabId, _service, _first, _firstSame, isPlaying) {
+		if (!isPlaying) {
+			return;
+		}
+		prev(tabId, _service);
+	});
 }
 
 function listenForHotKeys() {
-    $.ajax({
-        url: "http://localhost:18711/hotkeys/",
-    }).done(function(data) {
-        console.log(data)
-        if (data == 'pause') {
-            togglePause();
-        }
-        if (data == 'next') {
-            nextTrack();
-        }
-        if (data == 'prev') {
-            prevTrack();
-        }
-        listenForHotKeys();
-    }).error(function(data) {
-        console.log("hotKey service error " + data.status);
-        setTimeout(listenForHotKeys, 10000);
-    });
+	$.ajax({
+		url: "http://localhost:18711/hotkeys/",
+	}).done(function (data) {
+		console.log(data)
+		if (data == 'pause') {
+			togglePause();
+		}
+		if (data == 'next') {
+			nextTrack();
+		}
+		if (data == 'prev') {
+			prevTrack();
+		}
+		listenForHotKeys();
+	}).error(function (data) {
+		console.log("hotKey service error " + data.status);
+		chrome.alarms.create(null, {delayInMinutes: 0.1});
+		chrome.alarms.onAlarm.addListener(function () {
+			console.log("listenForHotKeys restarted");
+			chrome.alarms.clearAll();
+			listenForHotKeys();
+		});
+		// setTimeout(listenForHotKeys, 10000);
+	});
 }
 
 listenForHotKeys();
--- extension/popup.js
+++ extension/popup.js
@@ -1,144 +1,156 @@
 var services = [{
-    prefix: 'vk.com',
-    isPlaying: 'document.getElementsByClassName(\'top_audio_player_playing\').length > 0',
-    toggle: 'document.getElementsByClassName(\'top_audio_player_play\')[0].click();',
-    next: 'document.getElementsByClassName(\'top_audio_player_next\')[0].click();',
-    prev: 'document.getElementsByClassName(\'top_audio_player_prev\')[0].click();',
-    manipulateFirst: true
+	prefix: 'vk.com',
+	isPlaying: 'document.getElementsByClassName(\'top_audio_player_playing\').length > 0',
+	toggle: 'document.getElementsByClassName(\'top_audio_player_play\')[0].click();',
+	next: 'document.getElementsByClassName(\'top_audio_player_next\')[0].click();',
+	prev: 'document.getElementsByClassName(\'top_audio_player_prev\')[0].click();',
+	manipulateFirst: true
 }, {
-    prefix: 'www.mixcloud.com',
-    isPlaying: 'document.getElementsByClassName(\'pause-state\').length > 0',
-    toggle: 'document.getElementsByClassName(\'player-control\')[0].click();'
+	prefix: 'play.google.com/music',
+	isPlaying: 'document.getElementById(\'player-bar-play-pause\').className.indexOf(\'playing\')>=0',
+	toggle: 'document.getElementById(\'player-bar-play-pause\').click();',
+	next: 'document.getElementById(\'player-bar-forward\').click();',
+	prev: 'document.getElementById(\'player-bar-rewind\').click();'
 }, {
-    prefix: 'www.silver.ru',
-    isPlaying: '(!document.getElementsByTagName("audio")[0].paused)',
-    play: 'document.getElementsByTagName("audio")[0].play()',
-    pause: 'document.getElementsByTagName("audio")[0].pause()'
+	prefix: 'www.mixcloud.com',
+	isPlaying: 'document.getElementsByClassName(\'pause-state\').length > 0',
+	toggle: 'document.getElementsByClassName(\'player-control\')[0].click();'
 }, {
-    prefix: 'www.periscope.tv',
-    isPlaying: 'document.getElementsByClassName(\'is-playing\').length > 0',
-    toggle: 'document.getElementsByClassName(\'PlaybackControls\')[0].click();'
+	prefix: 'www.silver.ru',
+	isPlaying: '(!document.getElementsByTagName("audio")[0].paused)',
+	play: 'document.getElementsByTagName("audio")[0].play()',
+	pause: 'document.getElementsByTagName("audio")[0].pause()'
 }, {
-    prefix: 'soundcloud.com',
-    isPlaying: 'document.getElementsByClassName(\'playControl\').length > 0 && document.getElementsByClassName(\'playControl\')[0].className.indexOf(\'playing\')>=0',
-    toggle: 'document.getElementsByClassName(\'playControl\')[0].click();'
+	prefix: 'www.periscope.tv',
+	isPlaying: 'document.getElementsByClassName(\'is-playing\').length > 0',
+	toggle: 'document.getElementsByClassName(\'PlaybackControls\')[0].click();'
 }, {
-    prefix: 'www.youtube.com',
-    isPlaying: 'document.getElementsByClassName(\'html5-video-player\').length > 0 && document.getElementsByClassName(\'html5-video-player\')[0].className.indexOf(\'playing-mode\')>=0',
-    toggle: 'document.getElementsByClassName(\'ytp-play-button\')[0].click();'
+	prefix: 'soundcloud.com',
+	isPlaying: 'document.getElementsByClassName(\'playControl\').length > 0 && document.getElementsByClassName(\'playControl\')[0].className.indexOf(\'playing\')>=0',
+	toggle: 'document.getElementsByClassName(\'playControl\')[0].click();'
 }, {
-    prefix: 'play.spotify.com',
-    isPlaying: 'document.getElementById(\'play-pause\') && document.getElementById(\'play-pause\').className.indexOf(\'playing\')>=0',
-    toggle: 'document.getElementById(\'play-pause\').click();',
-    next: 'document.getElementById(\'next\').click();',
-    prev: 'document.getElementById(\'previous\').click();',
-    frameUrl: "play.spotify.com/apps/player"
+	prefix: 'www.youtube.com',
+	isPlaying: 'document.getElementsByClassName(\'html5-video-player\').length > 0 && document.getElementsByClassName(\'html5-video-player\')[0].className.indexOf(\'playing-mode\')>=0',
+	toggle: 'document.getElementsByClassName(\'ytp-play-button\')[0].click();'
+}, {
+	prefix: 'play.spotify.com',
+	isPlaying: 'document.getElementById(\'play-pause\') && document.getElementById(\'play-pause\').className.indexOf(\'playing\')>=0',
+	toggle: 'document.getElementById(\'play-pause\').click();',
+	next: 'document.getElementById(\'next\').click();',
+	prev: 'document.getElementById(\'previous\').click();',
+	frameUrl: "play.spotify.com/apps/player"
+}, {
+	prefix: '.podomatic.com',
+	isPlaying: 'document.getElementsByClassName(\'audio_play\').length > 0 && document.getElementsByClassName(\'audio_play\')[0].className.indexOf(\'pause\')>=0',
+	toggle: 'document.getElementsByClassName(\'audio_prev\')[0].click();',
+	next: 'document.getElementsByClassName(\'audio_play\')[0].click();',
+	prev: 'document.getElementsByClassName(\'audio_next\')[0].click();'
 }];
 
 function executeOnFrame(tabId, code, frameId, callBack) {
-    chrome.tabs.executeScript(tabId, {code: code, frameId: frameId || 0}, function(args) {
-        if (callBack) {
-            callBack(args[0]);
-        }
-    });
+	chrome.tabs.executeScript(tabId, {code: code, frameId: frameId || 0}, function (args) {
+		if (callBack) {
+			callBack(args[0]);
+		}
+	});
 }
 
 function getFrameId(tabId, frameUrl, callBack) {
-    if (!frameUrl) {
-        return callBack(0);
-    }
+	if (!frameUrl) {
+		return callBack(0);
+	}
 
-    chrome.webNavigation.getAllFrames({tabId: tabId}, function(frames) {
-        for (var i in frames) {
-            var frame = frames[i];
-            if (startsWith(frame.url, frameUrl)) {
-                callBack(frame.frameId);
-            }
-        }
-    });
+	chrome.webNavigation.getAllFrames({tabId: tabId}, function (frames) {
+		for (var i in frames) {
+			var frame = frames[i];
+			if (startsWith(frame.url, frameUrl)) {
+				callBack(frame.frameId);
+			}
+		}
+	});
 }
 
 function executeScript(tabId, code, frameUrl, callBack) {
-    getFrameId(tabId, frameUrl, function(frameId) {
-        executeOnFrame(tabId, code, frameId, callBack);
-    });
+	getFrameId(tabId, frameUrl, function (frameId) {
+		executeOnFrame(tabId, code, frameId, callBack);
+	});
 }
 
 function pause(tabId, service) {
-    if (service.hasOwnProperty("toggle")) {
-        executeScript(tabId, service.toggle, service.frameUrl);
-    } else {
-        executeScript(tabId, service.pause, service.frameUrl);
-    }
+	if (service.hasOwnProperty("toggle")) {
+		executeScript(tabId, service.toggle, service.frameUrl);
+	} else {
+		executeScript(tabId, service.pause, service.frameUrl);
+	}
 }
 
 function play(tabId, service) {
-    if (service.hasOwnProperty("toggle")) {
-        executeScript(tabId, service.toggle, service.frameUrl);
-    } else {
-        executeScript(tabId, service.play, service.frameUrl);
-    }
+	if (service.hasOwnProperty("toggle")) {
+		executeScript(tabId, service.toggle, service.frameUrl);
+	} else {
+		executeScript(tabId, service.play, service.frameUrl);
+	}
 }
 
 function toggleIcon() {
-    chrome.browserAction.getTitle({}, function(title) {
-        if (title == "play") {
-            chrome.browserAction.setIcon({path: "pause.png"});
-            chrome.browserAction.setTitle({title: "pause"});
-        } else {
-            chrome.browserAction.setIcon({path: "play.png"});
-            chrome.browserAction.setTitle({title: "play"});
-        }
-    });
+	chrome.browserAction.getTitle({}, function (title) {
+		if (title == "play") {
+			chrome.browserAction.setIcon({path: "pause.png"});
+			chrome.browserAction.setTitle({title: "pause"});
+		} else {
+			chrome.browserAction.setIcon({path: "play.png"});
+			chrome.browserAction.setTitle({title: "play"});
+		}
+	});
 }
 
 function getIsPlaying(tabId, service, callBack) {
-    chrome.tabs.executeScript(tabId, {code: service.isPlaying}, function(args) {
-        callBack(args[0]);
-    });
+	chrome.tabs.executeScript(tabId, {code: service.isPlaying}, function (args) {
+		callBack(args[0]);
+	});
 }
 
 function togglePause() {
-    toggleIcon();
+	toggleIcon();
 
-    chrome.tabs.query({}, function(tabs) {
-        console.log(tabs);
-        var first = true;
-        for (var i in tabs) {
-            var tab = tabs[i];
+	chrome.tabs.query({}, function (tabs) {
+		console.log(tabs);
+		var first = true;
+		for (var i in tabs) {
+			var tab = tabs[i];
 
-            for (var i in services) {
-                var service = services[i];
+			for (var i in services) {
+				var service = services[i];
 
-                var flag = tab.url.indexOf(service.prefix);
-                if (flag >= 0 && flag <= 10) {
-                    (function() {
-                        var tabId = tab.id;
-                        var _service = services[i];
-                        var _first = first;
-                        var _firstSame = !_service.visited;
-                        _service.visited = true;
+				var flag = tab.url.indexOf(service.prefix);
+				if (flag >= 0 && flag <= 10) {
+					(function () {
+						var tabId = tab.id;
+						var _service = services[i];
+						var _first = first;
+						var _firstSame = !_service.visited;
+						_service.visited = true;
 
-                        getIsPlaying(tabId, _service, function(isPlaying) {
-                            if (_service.manipulateFirst && !_firstSame) {
-                                return;
-                            }
+						getIsPlaying(tabId, _service, function (isPlaying) {
+							if (_service.manipulateFirst && !_firstSame) {
+								return;
+							}
 
-                            if (isPlaying) {
-                                pause(tabId, _service);
-                            }
+							if (isPlaying) {
+								pause(tabId, _service);
+							}
 
-                            if (_first && !isPlaying) {
-                                play(tabId, _service);
-                            }
-                        });
-                    })();
-                    first = false;
-                }
-            }
-        }
-    });
+							if (_first && !isPlaying) {
+								play(tabId, _service);
+							}
+						});
+					})();
+					first = false;
+				}
+			}
+		}
+	});
 }
 
 togglePause();
-setTimeout(window.close, 100);
+// setTimeout(window.close, 100);
